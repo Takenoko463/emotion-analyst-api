ca01ba2ed3975cd2adca52607b1be755d48fcc71
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-16 22:32:55 +0900

    恥に関する誤字修正

--- app/emotional_analysis.py
+++ app/emotional_analysis.py
@@ -1,4 +1,4 @@
-emotions = ["aware","shape","anger","unpleasant","fear","surprise","love","excitement","cheap","pleasant"]
+emotions = ["aware","shame","anger","unpleasant","fear","surprise","love","excitement","cheap","pleasant"]
 def get_emotional_words():
     emotional_words = {}
     for emotion in emotions:
